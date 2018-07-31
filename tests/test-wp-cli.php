6a238c60c0f95c590fc57e4babb186279a3b5aa3
Alain Schlesser <alain.schlesser@gmail.com>
2018-07-31 16:10:21 +0200

    Remove framework unit tests

--- tests/test-wp-cli.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class WP_CLI_Test extends PHPUnit_Framework_TestCase {
-
-	public function testGetPHPBinary() {
-		$this->assertSame( WP_CLI\Utils\get_php_binary(), WP_CLI::get_php_binary() );
-	}
-}
