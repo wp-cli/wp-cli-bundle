1f4c492cd8e59d32b8a8a5ede99e5d77225bc65b
GDR! <gdr@gdr.name>
2022-11-29 13:20:17 +0100

    Detect if running phar via CLI SAPI.
    Signed-off-by: GDR! <gdr@gdr.name>

--- php/boot-phar.php
+++ php/boot-phar.php
@@ -1,5 +1,14 @@
 <?php
 
+if ( 'cli' !== PHP_SAPI ) {
+	echo "Only PHP-CLI access.\n";
+	echo "You're currently using the " . PHP_SAPI . " PHP SAPI.\n";
+	echo "If you're trying to run this file with a web browser, don't.\n";
+	echo "When running in command line, ensure that `php -v` has the\n";
+	echo "word \"cli\" in the first line of output.\n";
+	die( -1 );
+}
+
 // Store the path to the Phar early on for `Utils\phar-safe-path()` function.
 define( 'WP_CLI_PHAR_PATH', getcwd() );
 
