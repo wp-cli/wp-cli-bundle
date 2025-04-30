e39a161e7ac90d2180c2d9cecd0a94ff050f27de
Pascal Birchler <pascalb@google.com>
2025-04-30 11:17:42 +0200

    Add missing dependency to built phar

--- utils/make-phar.php
+++ utils/make-phar.php
@@ -228,6 +228,7 @@ if ( 'cli' === BUILD ) {
 		->in( WP_CLI_VENDOR_DIR . '/nb/oxymel' )
 		->in( WP_CLI_VENDOR_DIR . '/psr' )
 		->in( WP_CLI_VENDOR_DIR . '/seld' )
+		->in( WP_CLI_VENDOR_DIR . '/marc-mabe/php-enum' ) // required by justinrainbow/json-schema.
 		->in( WP_CLI_VENDOR_DIR . '/justinrainbow/json-schema' )
 		->in( WP_CLI_VENDOR_DIR . '/gettext' )
 		->in( WP_CLI_VENDOR_DIR . '/mck89' )
